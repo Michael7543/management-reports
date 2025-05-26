239e53cf3948078dd4788c62be86b47e063eb557
Michael7543 <mdr.moran@yavirac.edu.ec>
2025-05-26 10:01:41 -0500

    [FEAT](Input): Add valueChange output event and update input handling

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,21 +1,27 @@
-import { Component, input, Input } from "@angular/core";
+import { Component, EventEmitter, input, Input, Output } from "@angular/core";
 import { InputTextModule } from "primeng/inputtext";
 
 @Component({
-  selector: 'app-input',
-  imports: [InputTextModule],
-  templateUrl: './input.component.html',
-  styleUrl: './input.component.scss',
+	selector: "app-input",
+	imports: [InputTextModule],
+	templateUrl: "./input.component.html",
+	styleUrl: "./input.component.scss",
 })
 export class InputComponent {
-  @Input() type: string = 'text';
-  @Input() placeholder: string = '';
-  @Input() disabled: boolean = false;
-  @Input() readonly?: boolean;
-  @Input() required?: boolean;
-  @Input() name?: string;
-  @Input() maxLength: number = 10;
-  @Input() minLength?: number;
-  @Input() autocomplete?: string;
-  @Input() classInput: string = '';
+	@Input() type: string = "text";
+	@Input() placeholder: string = "";
+	@Input() disabled: boolean = false;
+	@Input() readonly?: boolean;
+	@Input() required?: boolean;
+	@Input() name?: string;
+	@Input() maxLength: number = 10;
+	@Input() minLength?: number;
+	@Input() autocomplete?: string;
+	@Input() classInput: string = "";
+
+	@Output() valueChange = new EventEmitter<any>();
+	onInput(event: Event) {
+		const value = (event.target as HTMLInputElement).value;
+		this.valueChange.emit(value);
+	}
 }
