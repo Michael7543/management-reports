023986c0c1141018be38da8759216c9f67072413
Michael7543 <mdr.moran@yavirac.edu.ec>
2025-05-26 09:55:21 -0500

    [FEAT](Input): Implement Input component with dynamic properties and template

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, input, Input } from "@angular/core";
+import { InputTextModule } from "primeng/inputtext";
 
 @Component({
   selector: 'app-input',
-  imports: [],
+  imports: [InputTextModule],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss',
 })
 export class InputComponent {
-
+  @Input() type: string = 'text';
+  @Input() placeholder: string = '';
+  @Input() disabled: boolean = false;
+  @Input() readonly?: boolean;
+  @Input() required?: boolean;
+  @Input() name?: string;
+  @Input() maxLength: number = 10;
+  @Input() minLength?: number;
+  @Input() autocomplete?: string;
+  @Input() classInput: string = '';
 }
