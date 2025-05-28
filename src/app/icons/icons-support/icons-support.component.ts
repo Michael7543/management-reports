548ed08c66665fb89fd80b9d2df27dc0a9d68dad
Michael7543 <mdr.moran@yavirac.edu.ec>
2025-05-28 10:49:37 -0500

    [FEAT](Icons): Add IconsSupport and IconsWhatsapp components with SVG templates

---
+++ src/app/icons/icons-support/icons-support.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icons-support',
+  imports: [],
+  templateUrl: './icons-support.component.html',
+  styleUrl: './icons-support.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class IconsSupportComponent {
+
+}
---
+++ src/app/icons/icons-whatsapp/icons-whatsapp.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icons-whatsapp',
+  imports: [],
+  templateUrl: './icons-whatsapp.component.html',
+  styleUrl: './icons-whatsapp.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class IconsWhatsappComponent {
+
+}
