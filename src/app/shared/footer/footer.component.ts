3cbbef9c308ecb98217478bc0fbb24d621b74d7a
KevinRivera1 <shimpo11.kr@gmail.com>
2025-06-05 19:42:42 -0500

    [REFACTOR](styles): componentize SVGs into reusable components
    - Componentize SVGs
    -  Reorganize tailwindcss classes

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,8 +1,13 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { EmailIconComponent } from "@assets/icons/email-icon/email-icon.component";
+import { FacebookIconComponent } from "@assets/icons/facebook-icon/facebook-icon.component";
+import { PhoneIconComponent } from "@assets/icons/phone-icon/phone-icon.component";
+import { XIconComponent } from "@assets/icons/x-icon/x-icon.component";
+import { YoutubeIconComponent } from "@assets/icons/youtube-icon/youtube-icon.component";
 
 @Component({
 	selector: "app-footer",
-	imports: [],
+	imports: [PhoneIconComponent, EmailIconComponent, YoutubeIconComponent, XIconComponent, FacebookIconComponent],
 	templateUrl: "./footer.component.html",
 	styleUrl: "./footer.component.scss",
 	changeDetection: ChangeDetectionStrategy.OnPush,
---
+++ src/assets/icons/phone-icon/phone-icon.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+
+@Component({
+	selector: "app-phone-icon",
+	imports: [],
+	templateUrl: "./phone-icon.component.html",
+	styleUrl: "./phone-icon.component.scss",
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PhoneIconComponent {}
