5b98b76acd5aaf4b31ecd79d88763adca2430dac
Michael7543 <mdr.moran@yavirac.edu.ec>
2025-06-14 13:07:45 -0500

    [FEAT](Records): agregar interfaz Records para la gestión de datos generales

---
+++ src/app/domain/model/generalData/Records.ts
@@ -0,0 +1,12 @@
+export interface Records {
+    id: number;
+    fecha: Date;
+    type_event: string;
+    name_event: string;
+    user: string;
+    estado: string;
+    hora_inicio: Date;
+    hora_fin: Date;
+    funcion: string;
+    actividad: string
+}
