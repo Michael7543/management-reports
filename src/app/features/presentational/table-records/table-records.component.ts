ee0f39f7efb15f9b4f2adf2efe93609f32c02dd4
KevinRivera1 <shimpo11.kr@gmail.com>
2025-06-15 21:24:54 -0500

    [CHORE](table-records): Add new data and use Tag the primeng/tag

--- src/app/features/presentational/table-records/table-records.component.ts
+++ src/app/features/presentational/table-records/table-records.component.ts
@@ -1,34 +1,57 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { TableModule } from 'primeng/table';
-import { CommonModule } from '@angular/common';
-import { Records } from '@app/domain/model/generalData/Records';
-import { ButtonComponent } from "../../../shared/button/button.component";
-import { InputComponent } from "../../../shared/input/input.component";
+import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component, type OnInit } from "@angular/core";
+import type { Records } from "@domain/model/generalData/Records";
+import { ButtonComponent } from "@shared/button/button.component";
+import { InputComponent } from "@shared/input/input.component";
+import { TableModule } from "primeng/table";
+import { Tag } from "primeng/tag";
 
 @Component({
-  selector: 'app-table-records',
-  imports: [TableModule, CommonModule, ButtonComponent, InputComponent],
-  templateUrl: './table-records.component.html',
-  styleUrl: './table-records.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+	selector: "app-table-records",
+	imports: [TableModule, Tag, CommonModule, ButtonComponent, InputComponent],
+	templateUrl: "./table-records.component.html",
+	styleUrl: "./table-records.component.scss",
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TableRecordsComponent implements OnInit {
-  records: Records[] = [];
+	records: Records[] = [];
 
-  ngOnInit(): void {
-    this.records = [
-      {
-        id: 1,
-        fecha:new Date('2025-06-04'),
-        type_event: 'nu',
-        name_event: 'sd',
-        user: 'ds',
-        estado: 'ds',
-        hora_inicio: new Date('2025-06-04T08:30:00'),
-        hora_fin: new Date('2025-06-04T10:00:00'),
-        funcion: 'ds',
-        actividad: 'ds',
-      },
-    ];
-  }
+	ngOnInit(): void {
+		this.records = [
+			{
+				id: 1,
+				fecha: new Date("2025-06-04"),
+				type_event: "SOLICITUD",
+				name_event: "SEGURIDAD TRANSPORTE PUBLICO",
+				user: "JUAN LOSE DE MORALES LLUMISACA",
+				estado: "APROBADO",
+				hora_inicio: new Date("2025-06-04"),
+				hora_fin: new Date("2025-06-04"),
+				funcion: "OPERATIVO DE SEGURIDAD EN TRANSPORTE PÚBLICO",
+				actividad: "SEGURIDAD TRANSPORTE PUBLICO",
+			},
+		];
+	}
+
+	getSeverity(estado: string) {
+		switch (estado) {
+			case "unqualified":
+				return "danger";
+
+			case "qualified":
+				return "success";
+
+			case "new":
+				return "info";
+
+			case "negotiation":
+				return "warn";
+
+			case "renewal":
+				return null;
+
+			default:
+				return undefined;
+		}
+	}
 }
