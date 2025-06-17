e18d202c4bb54318b5f5a2898e2166e9a7ea38c2
KevinRivera1 <shimpo11.kr@gmail.com>
2025-06-17 17:57:46 -0500

    [CHORE]: Format paths relatives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,22 @@
 import { NgClass } from "@angular/common";
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { FooterComponent } from "./shared/footer/footer.component";
-import { HeaderComponent } from "./shared/header/header.component";
-import { SidebarComponent } from "./shared/sidebar/sidebar.component";
-import { TableRecordsComponent } from "./features/presentational/table-records/table-records.component";
+import { FooterComponent } from "@shared/footer/footer.component";
+import { HeaderComponent } from "@shared/header/header.component";
+import { SidebarComponent } from "@shared/sidebar/sidebar.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    SidebarComponent,
-    FooterComponent,
-    NgClass,
-    TableRecordsComponent,
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+	selector: "app-root",
+	imports: [RouterOutlet, HeaderComponent, SidebarComponent, FooterComponent, NgClass],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'management-reports';
+	title = "management-reports";
 
-  sidebarOpen = true;
+	sidebarOpen = true;
 
-  toggleSidebar() {
-    this.sidebarOpen = !this.sidebarOpen;
-  }
+	toggleSidebar() {
+		this.sidebarOpen = !this.sidebarOpen;
+	}
 }
